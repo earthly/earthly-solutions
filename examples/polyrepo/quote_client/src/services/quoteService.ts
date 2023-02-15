df5136a0f5d03e8611e1fe8559f320589501e2a3
Evan C Pease <evan.pease@gmail.com>
2023-02-15 13:34:12 -0800

    remove unused dir

--- examples/polyrepo/quote_client/src/services/quoteService.ts
+++
@@ -1,5 +0,0 @@
-export const getQuote = async (server: string) => {
-  const resp = await fetch(server)
-  const text = await resp.text()
-  return text
-}
--- examples/polyrepo/rust_server/README.md
+++
@@ -1,6 +0,0 @@
-# Earthly Rust Service Eampple
-
-```
-earthly +docker
-docker run -d --name rust_example -dp 8000:8000  ezeev/earthly-rust-example
-```
