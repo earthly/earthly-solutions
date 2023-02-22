42a7e08f7760ef9f28623f8385ca7aced8d574c3
Evan C Pease <evan.pease@gmail.com>
2023-02-22 15:02:12 -0800

    dynamic quotes v0.13

--- examples/polyglot-repo/node_server/src/quotes.js
+++ examples/polyglot-repo/node_server/src/quotes.js
@@ -1,16 +1,18 @@
+const fs = require('fs')
 
+function getQuotes() {
+  try {
+    const data = fs.readFileSync('quotes.txt', 'utf8')
+    return data.split("\n").filter(s => s !== '')
+  } catch(err) {
+    console.error(err)
+  }
+}
 
-const quotes = [
-  'You live and learn. At any rate, you live.',
-  'This is another random quote from Earthly',
-  'Progress is a nice word. But change is its motivator and change has its enemies.',
-  "It's the opinion of some that crops could be grown on the moon. Which raises the fear that it may not be long before we're paying somebody not to.",
-  'Perfect as the wing of a bird may be, it will never enable the bird to fly if unsupported by the air. Facts are the air of science. Without them a man of science can never rise.',
-]
+const quotes = getQuotes()
 
 function randomQuote() {
   return quotes[Math.floor(Math.random() * quotes.length)]
 }
 
-
-module.exports = { randomQuote, quotes}
+module.exports = { randomQuote, quotes, getQuotes }
--- examples/polyglot-repo/node_server/src/quotes.test.js
+++ examples/polyglot-repo/node_server/src/quotes.test.js
@@ -1,5 +1,9 @@
 const quotes = require('./quotes')
 
-test('randomQuotes gives a valid quote', () => {
+test('randomQuotes gives a valid quote', async () => {
+  
+  const data =quotes.getQuotes()
+  console.log(data)
+
   expect(quotes.quotes.includes(quotes.randomQuote())).toBe(true) 
 })
