c6e2f92715792c71192010b58af971a91fe30754
Evan C Pease <evan.pease@gmail.com>
2023-03-01 17:33:24 -0800

    demo 4

--- examples/polyglot-repo/go_server/main.go
+++ examples/polyglot-repo/go_server/main.go
@@ -7,24 +7,21 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-
 func main() {
 
-
-  quotes, err := Quotes() 
-  if err != nil {
-    logrus.Fatal(err)
-  }
+	quotes, err := Quotes()
+	if err != nil {
+		logrus.Fatal(err)
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Max-Age", "15")
-		//fmt.Fprint(w, RandomQuote(quotes))
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Max-Age", "15")
+		fmt.Fprint(w, RandomQuote(quotes))
 
-
-    logrus.Info(RandomQuote(quotes))
-    fmt.Fprint(w, "Hello again")
+		//logrus.Info(RandomQuote(quotes))
+		//fmt.Fprint(w, "Hello again")
 	})
 
 	http.ListenAndServe(":8001", nil)
