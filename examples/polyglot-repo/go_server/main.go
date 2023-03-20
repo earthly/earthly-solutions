ba4e71301325139bb76fdecf104acc449b524abb
Evan C Pease <evan.pease@gmail.com>
2023-03-20 14:18:37 -0700

    earthly ci demo

--- examples/polyglot-repo/go_server/main.go
+++ examples/polyglot-repo/go_server/main.go
@@ -21,9 +21,9 @@ func main() {
 		quote := RandomQuote(quotes)
 
 		logrus.Info(quote)
-		//fmt.Fprint(w, quote)
+		fmt.Fprint(w, quote)
 
-		fmt.Fprint(w, "Hello from Earthly CI - 2:13!")
+		//fmt.Fprint(w, "Hello from Earthly CI!")
 
 	})
 
